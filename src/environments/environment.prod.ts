3e9ca4afdd228aff383045e5182175e1db992d65
Mateusz Jablonowski <mateuszjablonowski@MacBook-Pro-Mateusz-3.local>
2023-11-08 19:15:43 +0100

    firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  useEmulators: false,
+  firebase: {
+    apiKey: "AIzaSyBaDcP_Dpt_k1YZZqS-oDJA1a2P-DCf1mU",
+    authDomain: "angular-firebase-app-6c0d9.firebaseapp.com",
+    projectId: "angular-firebase-app-6c0d9",
+    storageBucket: "angular-firebase-app-6c0d9.appspot.com",
+    messagingSenderId: "729558861211",
+    appId: "1:729558861211:web:e2313f357ed51ebcdc4d3e"
+  },
+  api: {
+    createUser: ""
+  }
+};
